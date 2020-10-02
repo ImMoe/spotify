61a6a081d595e8a2ff349b8154c860261fb17aa9
= <immoe98@gmail.com>
2020-10-02 19:58:24 +0200

    Adding content and songs

--- scripts/play.js
+++ scripts/play.js
@@ -4,20 +4,22 @@
 const playButtons = document.querySelectorAll('.playSong')
 
 playButtons.forEach((single) => {
-  single.addEventListener('click', (e) => {
-    const tableData = Array.from(e.target.parentElement.parentElement.children)
-    const [title, artist, , song, cover] = tableData
-    // Update player song info
-    songInfo[0].innerText = title.innerText
-    songInfo[1].innerText = artist.innerText
-    // Set the cover image
-    songImage.setAttribute('src', `songs/covers/${cover.innerText}.png`)
-    // Set the source for player
-    songPlayer.src = `songs/${song.innerText}.mp3`
-    songPlayer.load()
-    songPlayer.play()
-
-    // Update Page title to show song title and artist
-    document.title = ` ${title.innerText} - ${artist.innerText}`
-  })
+  single.addEventListener('click', startPlaySong)
 })
+
+function startPlaySong(e) {
+  const tableData = Array.from(e.target.parentElement.parentElement.children)
+  const [title, artist, , song, cover] = tableData
+  // Update player song info
+  songInfo[0].innerText = title.innerText
+  songInfo[1].innerText = artist.innerText
+  // Set the cover image
+  songImage.setAttribute('src', `songs/covers/${cover.innerText}.png`)
+  // Set the source for player
+  songPlayer.src = `songs/${song.innerText}.mp3`
+  songPlayer.load()
+  songPlayer.play()
+
+  // Update Page title to show song title and artist
+  document.title = ` ${title.innerText} - ${artist.innerText}`
+}
--- scripts/songs.js
+++ scripts/songs.js
@@ -1,23 +1,38 @@
 const songReference = [
   {
-    id: 1,
     title: 'Since Then',
     artist: 'P Montana, Sona, Baseman',
     song: '1. Since Then',
     cover: 'since then',
   },
   {
-    id: 2,
     title: 'Creep On Me',
     artist: 'Gashi, French Montana, DJ Snake',
     song: '2. Creep on me',
     cover: 'creep on me',
   },
   {
-    id: 3,
     title: 'Akhi',
     artist: 'Dree Low',
     song: '3. Akhi',
     cover: 'akhi',
+  },
+  {
+    title: 'Har du vatt här',
+    artist: 'GULEED',
+    song: '4. Har du vatt här',
+    cover: 'har du vatt här',
+  },
+  {
+    title: 'Faster Than Fast',
+    artist: 'Quada',
+    song: '5. Faster than fast',
+    cover: 'faster than fast',
+  },
+  {
+    title: "Lartiste - C'est une frappe",
+    artist: 'Lartiste',
+    song: "6. C'est une frappe",
+    cover: 'cest une frappe',
   },
 ]
